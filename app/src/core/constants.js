218833b3eb421306a1961deb209b3fcf8f3c1ade
jgpATs2w <jgp@sens2web.es>
2016-05-13 21:01:06 +0200

    moved to angular design

---
+++ app/src/core/constants.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.core')
+    .constant('FIREBASE_URL', 'https://waitandeat-v2-demo.firebaseio.com/')
+    .constant('PROTECTED_PATHS', ['/waitlist']);
+
+})();
