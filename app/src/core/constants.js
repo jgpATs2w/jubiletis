00c54110aaf7ef30440663a1e629ca07b500c55b
javier <jgp@sens2web.es>
2016-06-16 09:16:28 +0200

    cambiados mensajes

--- app/src/core/constants.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('app.core')
-    .constant('FIREBASE_URL', 'https://waitandeat-v2-demo.firebaseio.com/')
-    .constant('PROTECTED_PATHS', ['/waitlist']);
-
-})();
